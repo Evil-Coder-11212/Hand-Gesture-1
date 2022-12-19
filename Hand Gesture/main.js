b0e027eef307736b9c54a0e52bc22332a5577d69
Evil-Coder-11212 <99388161+Evil-Coder-11212@users.noreply.github.com>
2022-12-19 18:16:28 +0530

    Add files via upload

---
+++ Hand Gesture/main.js
@@ -0,0 +1,32 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 180
+})
+
+const cameraEl = document.querySelector("#live-camera");
+const capturedImage = document.querySelector("#captured-image");
+Webcam.attach(cameraEl)
+const takeScreenShot = () =>{
+    Webcam.snap(dataURL =>{
+        document.querySelector("#result").innerHTML = `<img src=${dataURL} id=""captured-image />`    
+    })
+}
+classifier = ml5.imageClassify("https://teachablemachine.withgoogle.com/models/T4a6W7oUF", () =>{
+    console.log("Loaded")
+})
+
+const check = () =>{
+    classifier.classify(capturedImage, gotResult)
+}
+
+const gotResult = (error, result) =>{
+    if(error){
+        console.log(error)
+    }else{
+        gesture = result[0].label;
+
+
+    }
+} 
